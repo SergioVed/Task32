d1270f258d1e146c770a63c0f922570a518b3d5f
SergioVed <vedankinsergij@gmail.com>
2023-12-09 13:01:29 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Button } from './components/button';
+import { User } from './components/user';
 
 function App() {
+const [buttonValue, setButtonValue] = useState("I am not looged in")
+const [inputValue, setInputValue] = useState()
+
+  const [users, setUsers] = useState([])
+
+  const Log = () => {
+    const newUser = {
+      name: inputValue,
+      secondname: "Vediankin",
+      isLoggedIn: !users[0]?.isLoggedIn
+    }
+    setUsers([newUser])
+    setButtonValue(newUser.isLoggedIn ? "i am not logged in" : "I am logged in")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {/* <button onClick={Log}>I am not logged in</button>
+        <button onClick={notLog}>I am logged in</button> */}
+        <Button onClickFunc={Log} value={buttonValue}/>
+      </div>
+      <div>
+        <input value={inputValue} onChange={(event) => {setInputValue(event.target.value)}}></input>
+        {users.map((user) => (
+          <div>
+            {user.isLoggedIn ? (
+              <p>U good <User name={user.name} secondname={user.secondname}/></p>
+
+            ):(
+              <p>Please log in <User name={user.name} secondname={user.secondname}/></p>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/user/index.jsx
@@ -0,0 +1,5 @@
+export const User = (props) => {
+    return(
+        <span>{props.name}{props.secondname}{props.isloggedin}</span>
+    )
+}
